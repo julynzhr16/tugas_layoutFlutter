ebaeb56bacbf4886f3e6ee1a1e20dcd5b54e09d7
julynazhr16 <sitijuliyantii06@gmail.com>
2025-09-26 19:35:49 +0700

    update

---
+++ tugas4.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class tugas4 extends StatelessWidget {
+  const tugas4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    "Training",
+                    style: TextStyle(
+                      fontSize: 22, fontWeight: FontWeight.bold,
+                    ),
+                  ), 
+                ),
+                Row(children: [
+                  Icon(Icons.chevron_left),
+                  Icon(Icons.calendar_month),
+                  Icon(Icons.chevron_right),
+                ],
+                ),
+              ]
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
